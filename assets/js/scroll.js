dbf83c01c03e23d202f0750d0c62413f69f5d0a5
OlgaLosikova <techtigra@gmail.com>
2024-01-30 18:41:51 +0300

    removing unnecessary

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,7 +2,6 @@ document.querySelectorAll('a[href^="#"').forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
     let href = link.getAttribute("href").substring(1);
-    console.log(href);
     const scrollTarget = document.getElementById(href);
     const elementPosition = scrollTarget.getBoundingClientRect().top;
     window.scrollBy({ top: elementPosition, behavior: "smooth" });
